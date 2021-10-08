4828bcdf92e8a6c1429c82cdf2b40e97e62c51a6
delangle <flaviendelangle@gmail.com>
2021-10-08 17:03:34 +0200

    Work

--- packages/grid/_modules_/grid/hooks/features/rowGroupByColumns/gridRowGroupByColumnsGroupColDef.tsx
+++ packages/grid/_modules_/grid/hooks/features/rowGroupByColumns/gridRowGroupByColumnsGroupColDef.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { GridRowGroupByColumnsGroupingCell } from '../../../components/cell/GridRowGroupingGroupingCell';
+import { GridRowGroupByColumnsGroupingCell } from '../../../components/cell/GridRowGroupByColumnsGroupingCell';
 import { GridColDef } from '../../../models/colDef/gridColDef';
 import { GRID_STRING_COL_DEF } from '../../../models/colDef/gridStringColDef';
 
 export const GRID_ROW_GROUP_BY_COLUMNS_GROUP_COL_DEF: GridColDef = {
   ...GRID_STRING_COL_DEF,
-  field: '__row_grouping_data_group__',
+  field: '__row_group_by_columns_group',
   sortable: false,
   filterable: false,
   disableColumnMenu: true,
